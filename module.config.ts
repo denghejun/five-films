bfa1e76a1c48b96d6900ecf33aacd624e9ed6e56
Denghejun <hjdeng@CNhjdeng.local>
2017-12-15 20:00:38 +0800

    update package json

--- module.config.ts
+++ module.config.ts
@@ -2,7 +2,7 @@ import * as Modules from './src/modules'
 import { ServiceContract } from 'react-native-modular-bootstrapper'
 
 export default class AppModuleProvider implements ServiceContract.ModuleProvider {
-  public registerModules(): any[] {
+  public registerModules(): (new () => ServiceContract.Module)[] {
     return [Modules.ServiceModule];
   }
 }
